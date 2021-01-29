0082dd799859b4b8cb13cfa1e8b83c1c8b1992e1
PatrickGe <pattigebhardt@googlemail.com>
2021-01-29 13:15:55 +0100

    adding .exe for 0.1 version

---
+++ Molekülbaukasten/Assets/Skripte/Logger1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+public class Logger1
+{
+
+    public Logger1(string logFilePath)
+    {
+        if (!logFilePath.EndsWith(".log"))
+            logFilePath += ".log";
+        LogFilePath = logFilePath;
+        if (!File.Exists(LogFilePath))
+            File.Create(LogFilePath).Close();
+        WriteLine("New Session Started");
+    }
+
+    public string LogFilePath { get; private set; }
+
+    public void WriteLine(object message)
+    {
+        using (StreamWriter writer = new StreamWriter(LogFilePath, true))
+            writer.WriteLine(DateTime.Now.ToString() + ": " + message.ToString());
+    }
+
+}
